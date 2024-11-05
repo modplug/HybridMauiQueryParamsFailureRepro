679e868ac912f7518e4582c6ea303d242231f12b
Erlend Angelsen <erlend.angelsen@gmail.com>
2024-11-05 14:16:06 +0100

    add files

--- DeeplinkTest/Extensions/UriExtensions.cs
+++ DeeplinkTest/Extensions/UriExtensions.cs
@@ -1,6 +1,16 @@
 namespace DeeplinkTest.Extensions;
 
-public class UriExtensions
+internal static class UriExtensions
 {
-    
+    internal static bool IsBaseOfPage(this Uri baseUri, string? uriString)
+    {
+        if (Path.HasExtension(uriString))
+        {
+            // If the path ends in a file extension, it's not referring to a page.
+            return false;
+        }
+
+        var uri = new Uri(uriString!);
+        return baseUri.IsBaseOf(uri);
+    }
 }
